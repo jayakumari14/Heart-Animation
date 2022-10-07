f4ada90a04addcb4083da2cd4df9aec54da9c571
jaya Kumari <78198766+jayakumari14@users.noreply.github.com>
2022-10-07 16:52:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const body = document.querySelector("body");
+body.addEventListener("mousemove", (e) => {
+  const xpos = e.offsetX;
+  const ypos = e.offsetY;
+  const span1 = document.createElement("span");
+  span1.style.left = xpos + "px";
+  span1.style.top = ypos + "px";
+  body.appendChild(span1);
+
+  const size = Math.random() * 100;
+  span1.style.width = size + "px";
+  span1.style.height = size + "px";
+  setTimeout(() => {
+    span1.remove();
+  }, 3000);
+});
